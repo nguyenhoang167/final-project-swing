1a0348c6e2e9c3138f132acd245570e47f97dd7a
nguyenhoang167 <hoang.nguyen150013@vnuk.edu.vn>
2019-05-19 23:51:25 +0700

    Edit view + edit readme

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # vnuk employees management
 Create environment variables named DB_USR & DB_PWD : these will be the credentials to access your DB through your ConnectionFactory
 
-Run SQLRunAllQueries.java
+Run file database into mysql
+
+run file Application.java in eclipse
--- src/vn/edu/vnuk/em/view/UpdateBasicSalary.java
+++ src/vn/edu/vnuk/em/view/UpdateBasicSalary.java
@@ -52,42 +52,26 @@ public class UpdateBasicSalary extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnNewButton = new JButton("Add New Employee");
-		btnNewButton.setBounds(0, 0, 147, 150);
-		contentPane.add(btnNewButton);
-		
-		JButton btnModifyListOf = new JButton("Modify List Employee");
-		btnModifyListOf.setBounds(0, 146, 147, 150);
-		contentPane.add(btnModifyListOf);
-		
-		JButton btnUpdateBasicSalary = new JButton("Update Basic Salary");
-		btnUpdateBasicSalary.setBounds(0, 292, 147, 150);
-		contentPane.add(btnUpdateBasicSalary);
-		
-		JButton btnExit = new JButton("Exit");
-		btnExit.setBounds(0, 439, 147, 150);
-		contentPane.add(btnExit);
-		
 		textField = new JTextField();
-		textField.setBounds(431, 34, 207, 26);
+		textField.setBounds(416, 34, 207, 26);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
 		JLabel lblNewLabel = new JLabel("CURRENT BASIC SALARY:");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		lblNewLabel.setBounds(200, 27, 187, 40);
+		lblNewLabel.setBounds(185, 27, 187, 40);
 		contentPane.add(lblNewLabel);
 		
 		JButton btnNewButton_1 = new JButton("EDIT");
-		btnNewButton_1.setBounds(424, 532, 131, 40);
+		btnNewButton_1.setBounds(209, 532, 131, 40);
 		contentPane.add(btnNewButton_1);
 		
 		JButton btnApply = new JButton("APPLY");
-		btnApply.setBounds(632, 532, 131, 40);
+		btnApply.setBounds(469, 532, 131, 40);
 		contentPane.add(btnApply);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(159, 69, 641, 451);
+		scrollPane.setBounds(12, 69, 774, 451);
 		contentPane.add(scrollPane);
 		
 		String[] columns = {"ID", "Name", "Salary"};
